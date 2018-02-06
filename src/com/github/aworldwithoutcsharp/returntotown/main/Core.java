1a898eb3f9c01a6b993dde73bdab6453ce100052
Caleb Sacks <calebjs88@gmail.com>
2018-02-06 16:21:47 -0500

    add base project

---
+++ src/com/github/aworldwithoutcsharp/returntotown/main/Core.java
@@ -0,0 +1,11 @@
+package com.github.aworldwithoutcsharp.returntotown.main;
+
+import java.util.Scanner;
+
+public class Core {
+    Scanner in = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+
+    }
+}
