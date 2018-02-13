f0b1eb349bb7c17304e3d82f8b2071fe3f4f5705
Caleb Sacks <calebjs88@gmail.com>
2018-02-13 14:47:54 -0500

    start plan

--- src/com/github/aworldwithoutcsharp/returntotown/main/Core.java
+++ src/com/github/aworldwithoutcsharp/returntotown/main/Core.java
@@ -2,9 +2,34 @@ package com.github.aworldwithoutcsharp.returntotown.main;
 
 import java.util.Scanner;
 
+/*
+ * TODO: {
+ *     Mechanics:
+ *       - input:
+ *         - command-based
+ *       - output:
+ *         - details / paragraph
+ *         - possibly formatted keywords??
+ *     I. Tutorial
+ *     II. Game
+ *       A. starts out in forest
+ *       B. searches for clues
+ *       C. discovers beginning
+ * }
+ */
+
 public class Core {
-    Scanner in = new Scanner(System.in);
-    
+    private static final String PROMPT_SEPARATOR = "";
+
+    private static Scanner in = new Scanner(System.in);
+    public static String input() {
+        return in.nextLine();
+    }
+    public static String input(String prompt) {
+        System.out.print(prompt + PROMPT_SEPARATOR);
+        return in.nextLine();
+    }
+
     public static void main(String[] args) {
 
     }
