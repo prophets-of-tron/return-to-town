897af8cc470748443caaf6919e4c086077611f4b
Caleb Sacks <calebjs88@gmail.com>
2018-02-27 15:38:06 -0500

    fixed merge issues 0000

--- src/com/github/aworldwithoutcsharp/returntotown/main/scenes/Tutorial.java
+++ src/com/github/aworldwithoutcsharp/returntotown/main/scenes/Tutorial.java
@@ -11,15 +11,9 @@ public class Tutorial {
 
     public static void run() {
         System.out.println(
-                "Welcome to the tutorial!" + " " +
-<<<<<<< HEAD
-                        "This scene is designed to familiarize yourself with the text interface of this game...");
+                "Welcome to the tutorial!" +
+                 "This scene is designed to familiarize yourself with the text interface of this game... You will begin in a ballroom at a gala, with PATRONS of your pharmaceutical company all around. There is also a POSTER nearby, and there is a waiter coming your way with a tray of CHAMPAGNE. Try Interacting with the POSTER.");
         Core.UserCommand input = null;
-=======
-                        "This scene is designed to familiarize yourself with the text interface of this game... You will begin in a ballroom at a gala, with PATRONS of your pharmaceutical company all around. There is also a POSTER nearby, and there is a waiter coming your way with a tray of CHAMPAGNE. Try Interacting with the POSTER.");
-        Core.Command input = null;
->>>>>>> origin/master
-
         while (input != null ? input.getDefinition() != Core.CommandDefinition.EXIT : true) {
             String output = createOutput();
             System.out.println(output);
@@ -29,9 +23,9 @@ public class Tutorial {
                                                 // defined #perform(); see Core.CommandDefinition
         }
         while (input.equals("Interact(POSTER)")) {
-            String output = createOutput("The poster reads, 'Nice Job! You not only can follow simple instructions, but you can also read! Now remember, you are at a party, so stop reading signs and ionteract with the PATRONS you invited!'");
+            String output = "The poster reads, 'Nice Job! You not only can follow simple instructions, but you can also read! Now remember, you are at a party, so stop reading signs and ionteract with the PATRONS you invited!'";
             System.out.println(output);
-            input == Core.input();
+            input = Core.input();
         }
     }
 }
