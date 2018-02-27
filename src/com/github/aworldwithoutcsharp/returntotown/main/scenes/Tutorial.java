8d6ae9a84dcfdb949997bdaf835e8808556ca02b
Caleb Sacks <calebjs88@gmail.com>
2018-02-27 15:23:20 -0500

    refactor command data structures

--- src/com/github/aworldwithoutcsharp/returntotown/main/scenes/Tutorial.java
+++ src/com/github/aworldwithoutcsharp/returntotown/main/scenes/Tutorial.java
@@ -13,12 +13,15 @@ public class Tutorial {
         System.out.println(
                 "Welcome to the tutorial!" + " " +
                         "This scene is designed to familiarize yourself with the text interface of this game...");
-        Core.Command input = null;
+        Core.UserCommand input = null;
 
-        while (input != null ? input.getType() != Core.CommandType.EXIT : true) {
+        while (input != null ? input.getDefinition() != Core.CommandDefinition.EXIT : true) {
             String output = createOutput();
             System.out.println(output);
             input = Core.input();
+
+            input.getDefinition().perform();    // will only do anything for the enum instances that have explicitely
+                                                // defined #perform(); see Core.CommandDefinition
         }
     }
 }
