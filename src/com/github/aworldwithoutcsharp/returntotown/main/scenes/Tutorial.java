7cd8934016c917dd8c76a1c36bf8c555bad22deb
Caleb Sacks <calebjs88@gmail.com>
2018-02-27 15:39:20 -0500

    added ' '

--- src/com/github/aworldwithoutcsharp/returntotown/main/scenes/Tutorial.java
+++ src/com/github/aworldwithoutcsharp/returntotown/main/scenes/Tutorial.java
@@ -11,7 +11,7 @@ public class Tutorial {
 
     public static void run() {
         System.out.println(
-                "Welcome to the tutorial!" +
+                "Welcome to the tutorial!" + " " +
                  "This scene is designed to familiarize yourself with the text interface of this game... You will begin in a ballroom at a gala, with PATRONS of your pharmaceutical company all around. There is also a POSTER nearby, and there is a waiter coming your way with a tray of CHAMPAGNE. Try Interacting with the POSTER.");
         Core.UserCommand input = null;
         while (input != null ? input.getDefinition() != Core.CommandDefinition.EXIT : true) {
